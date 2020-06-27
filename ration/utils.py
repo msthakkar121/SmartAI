df82a97ea793335d0279f140ccb96db4bfabb982
Mohit Thakkar <mohit.thakkar@thegatewaycorp.com>
2020-06-27 13:19:45 +0530

    Updated Initial Data Saving Mechanism

--- ration/utils.py
+++ ration/utils.py
@@ -4,7 +4,13 @@ from django.utils.timezone import make_aware
 
 def insert_initial_data(sender, **kwargs):
     from ration.models import TaskExecutionTimings
-    TaskExecutionTimings.objects.get_or_create(task_name='GetCandidateDetails',
-                                               last_execution_time=make_aware(datetime.now()))
-    TaskExecutionTimings.objects.get_or_create(task_name='GetRequirementDetails',
-                                               last_execution_time=make_aware(datetime.now()))
+
+    obj, created = TaskExecutionTimings.objects.get_or_create(task_name='GetCandidateDetails')
+    if created:
+        obj.last_execution_time=make_aware(datetime.now())
+        obj.save()
+
+    obj, created = TaskExecutionTimings.objects.get_or_create(task_name='GetRequirementDetails')
+    if created:
+        obj.last_execution_time=make_aware(datetime.now())
+        obj.save()
