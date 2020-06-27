d980b0fcf9ffbde7bca23317df43cb9522b4b7df
Mohit Thakkar <mohitthakkar@rangam.com>
2020-06-27 12:46:21 +0530

    Activated Admin App & Updated Time Zone in Settings

--- smart_ai/urls.py
+++ smart_ai/urls.py
@@ -1,7 +1,7 @@
-# from django.contrib import admin
-# from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     # Don't need the admin app, might add later
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
