f3023f597b72827fad48cc873518b44478488a58
Mohit Thakkar <mohitthakkar@rangam.com>
2020-07-25 11:53:26 +0530

    Update Candidate Scoring Mechanism

--- knight/py_scripts/score_candidates.py
+++ knight/py_scripts/score_candidates.py
@@ -20,7 +20,7 @@ class ScoreCandidates:
         cleantext = cleantext.lower()
 
         # Remove certain characters
-        chars = ['\n', '\t', '. ', ', ', ',', ': ', ':', '? ', '?']
+        chars = ['\n', '\t', '\r', '&nbsp;', '"', '. ', ', ', ',', ': ', ':', '? ', '?']
         for char in chars:
             cleantext = cleantext.replace(char, ' ')
 
@@ -71,18 +71,50 @@ class ScoreCandidates:
         return re_skills
 
     def match_candidate_skills(self, re_skills, candidates):
+        # Pick relevant features from candidate data
+        candidates = candidates[
+            ['CandidateID', 'CandidateName', 'ResumeContent', 'CandidateSkills', 'ZIPCode', 'State']]
+        candidates['CandidateSkills'].fillna(' ', inplace=True)
+
+        # This feature is for candidate score in regards to the requirement we are processing
+        candidates['PercentageScore'] = ''
+
+        for i in candidates.index:
+            # Clean candidates data
+            candidates['ResumeContent'].loc[i] = self.cleanhtml(candidates['ResumeContent'].loc[i])
+            candidates['CandidateSkills'].loc[i] = self.cleanhtml(candidates['CandidateSkills'].loc[i])
+            candidates['PercentageScore'].loc[i] = self.get_percentage_score(re_skills,
+                                                                             candidates['ResumeContent'].loc[i] + ' ' +
+                                                                             candidates['CandidateSkills'].loc[i])
+
         return candidates
 
-    def score_candidates(self, requirement, candidates):
+    def get_percentage_score(self, skills, resume_data):
+        percentage_score = 0
+        total_skills_count = len(skills)
+        matching_skills_count = 0
 
+        for skill in skills:
+            skill = str(skill).lower()
+            if re.search(r'\b' + re.escape(skill) + r'\b', resume_data):
+                matching_skills_count = matching_skills_count + 1
+
+        percentage_score = (matching_skills_count * 100) / total_skills_count
+        return percentage_score
+
+    def score_candidates(self, requirement, candidates):
+        # Clean job data
         requirement['RequirementJobDescription'].loc[0] = self.cleanhtml(
             requirement['RequirementJobDescription'].loc[0])
         requirement['ActualRequirement'].loc[0] = self.cleanhtml(requirement['ActualRequirement'].loc[0])
         requirement['JobTitle'].loc[0] = self.cleanhtml(requirement['JobTitle'].loc[0])
 
+        # Get a list of skills from job data
         re_skills = self.get_requirement_skills(
             requirement['RequirementJobDescription'].loc[0] + ' ' + requirement['JobTitle'].loc[0])
 
+        # Get a list of candidates suitable or this job along with their score,
+        # which indicates their relevance to the job
         candidates = self.match_candidate_skills(re_skills, candidates)
 
         print('Scoring candidates is work in progress...!!!')
