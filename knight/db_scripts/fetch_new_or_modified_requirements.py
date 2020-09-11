5e2602551cb70781946595e93227b48eaca239df
Mohit Thakkar <mohitthakkar@rangam.com>
2020-09-11 11:37:25 +0530

    Replace database queries with stored procedures

--- knight/db_scripts/fetch_new_or_modified_requirements.py
+++ knight/db_scripts/fetch_new_or_modified_requirements.py
@@ -2,9 +2,10 @@ __author__ = "Mohit Thakkar"
 
 import pyodbc
 import pandas as pd
+import numpy as np
 from datetime import datetime, timedelta
-
 from django.conf import settings
+
 from ration.models import TaskExecutionTimings
 
 
@@ -36,67 +37,80 @@ class FetchNewOrModifiedRequirements:
         #    So the Modified date should be between 12 (3 hours prior to last execution time)
         #    and 12:15 (add the difference between current time & last execution time)
 
-        query = """
-                SELECT  
-                    rm.RequirementID,
-                    jtm.JobTitleText AS JobTitle,
-                    jtm2.JobTypeText AS JobType,
-                    rm.IsRemoteLocation,
-                    vm.VisaName AS VisaStatus,
-                    rm.RequirementJobDescription,
-                    rm.ActualRequirement,
-                    reverse(stuff(reverse(CONCAT(
-                        CASE
-                        WHEN rs.MandatorySkill1 IS NULL
-                            THEN ''
-                            ELSE rs.MandatorySkill1 + ', '
-                        END,
-                        CASE
-                        WHEN rs.MandatorySkill2 IS NULL
-                            THEN ''
-                            ELSE rs.MandatorySkill2 + ', '
-                        END,
-                        CASE
-                        WHEN rs.MandatorySkill3 IS NULL
-                            THEN ''
-                            ELSE rs.MandatorySkill3 + ', '
-                        END,
-                        CASE
-                        WHEN rs.MandatorySkill4 IS NULL
-                            THEN ''
-                            ELSE rs.MandatorySkill4 + ', '
-                        END)), 1, 2, '')) AS RequirementSkills,
-                    Stuff(
-                        (SELECT DISTINCT ', ' +zm.ZIPCode
-                        FROM dbo.RequirementMaster rm2
-                        LEFT JOIN dbo.RequirementLocation rl ON rm2.RequirementID = rl.RequirementID
-                        LEFT JOIN dbo.RequirementOtherLocation rol ON rm2.RequirementID = rol.RequirementID			
-                        LEFT JOIN dbo.AddressMaster am ON rl.AddressID = am.AddressID OR rol.AddressID = am.AddressID
-                        LEFT JOIN dbo.ZIPCodeMaster zm ON  am.ZIPCodeID = zm.ZIPCodeID
-                        WHERE rm.RequirementID = rm2.RequirementID  for xml path ('')),1,1,'') AS RequirementZIPCode,
-                    rm.CreatedDate,
-                    rm.ModifiedDate
-                FROM
-                    dbo.RequirementMaster rm WITH (NOLOCK)
-                LEFT JOIN
-                    dbo.JobTitleMaster jtm WITH (NOLOCK) ON rm.JobTitleID = jtm.JobTitleID
-                LEFT JOIN
-                    dbo.JobTypeMaster jtm2 WITH (NOLOCK) ON rm.JobTypeID = jtm2.JobTypeID
-                LEFT JOIN 
-                    dbo.RequirementVisa rv WITH (NOLOCK) ON rm.RequirementID = rv.RequirementID
-                LEFT JOIN
-                    dbo.VisaMaster vm WITH (NOLOCK) ON rv.VisaID = vm.VisaID
-                LEFT JOIN
-                    dbo.RequirementSkill rs WITH (NOLOCK) ON rm.RequirementID = rs.RequirementID
-                LEFT JOIN
-                    dbo.RequirementStatusTxn rst WITH (NOLOCK) ON rs.RequirementID = rst.RequirementID
-                WHERE
-                    (rm.CreatedDate > '{0}' OR rm.ModifiedDate BETWEEN '{1}' AND '{2}')
-                AND 
-                    rst.StatusID IN (1,6,10,11)
-                """.format(last_executed_time, start_modified_time, end_modified_time)
+        # query = """
+        #         SELECT
+        #             rm.RequirementID,
+        #             jtm.JobTitleText AS JobTitle,
+        #             jtm2.JobTypeText AS JobType,
+        #             rm.IsRemoteLocation,
+        #             vm.VisaName AS VisaStatus,
+        #             rm.RequirementJobDescription,
+        #             rm.ActualRequirement,
+        #             reverse(stuff(reverse(CONCAT(
+        #                 CASE
+        #                 WHEN rs.MandatorySkill1 IS NULL
+        #                     THEN ''
+        #                     ELSE rs.MandatorySkill1 + ', '
+        #                 END,
+        #                 CASE
+        #                 WHEN rs.MandatorySkill2 IS NULL
+        #                     THEN ''
+        #                     ELSE rs.MandatorySkill2 + ', '
+        #                 END,
+        #                 CASE
+        #                 WHEN rs.MandatorySkill3 IS NULL
+        #                     THEN ''
+        #                     ELSE rs.MandatorySkill3 + ', '
+        #                 END,
+        #                 CASE
+        #                 WHEN rs.MandatorySkill4 IS NULL
+        #                     THEN ''
+        #                     ELSE rs.MandatorySkill4 + ', '
+        #                 END)), 1, 2, '')) AS RequirementSkills,
+        #             Stuff(
+        #                 (SELECT DISTINCT ', ' +zm.ZIPCode
+        #                 FROM dbo.RequirementMaster rm2
+        #                 LEFT JOIN dbo.RequirementLocation rl ON rm2.RequirementID = rl.RequirementID
+        #                 LEFT JOIN dbo.RequirementOtherLocation rol ON rm2.RequirementID = rol.RequirementID
+        #                 LEFT JOIN dbo.AddressMaster am ON rl.AddressID = am.AddressID OR rol.AddressID = am.AddressID
+        #                 LEFT JOIN dbo.ZIPCodeMaster zm ON  am.ZIPCodeID = zm.ZIPCodeID
+        #                 WHERE rm.RequirementID = rm2.RequirementID  for xml path ('')),1,1,'') AS RequirementZIPCode,
+        #             rm.CreatedDate,
+        #             rm.ModifiedDate
+        #         FROM
+        #             dbo.RequirementMaster rm WITH (NOLOCK)
+        #         LEFT JOIN
+        #             dbo.JobTitleMaster jtm WITH (NOLOCK) ON rm.JobTitleID = jtm.JobTitleID
+        #         LEFT JOIN
+        #             dbo.JobTypeMaster jtm2 WITH (NOLOCK) ON rm.JobTypeID = jtm2.JobTypeID
+        #         LEFT JOIN
+        #             dbo.RequirementVisa rv WITH (NOLOCK) ON rm.RequirementID = rv.RequirementID
+        #         LEFT JOIN
+        #             dbo.VisaMaster vm WITH (NOLOCK) ON rv.VisaID = vm.VisaID
+        #         LEFT JOIN
+        #             dbo.RequirementSkill rs WITH (NOLOCK) ON rm.RequirementID = rs.RequirementID
+        #         LEFT JOIN
+        #             dbo.RequirementStatusTxn rst WITH (NOLOCK) ON rs.RequirementID = rst.RequirementID
+        #         WHERE
+        #             (rm.CreatedDate > '{0}' OR rm.ModifiedDate BETWEEN '{1}' AND '{2}')
+        #         AND
+        #             rst.StatusID IN (1,6,10,11)
+        #         """.format(last_executed_time, start_modified_time, end_modified_time)
+        #
+        # df = pd.read_sql_query(query, self.connection)
 
-        df = pd.read_sql_query(query, self.connection)
+        column_names = ['RequirementID', 'JobTitle', 'JobType', 'IsRemoteLocation', 'VisaStatus',
+                        'RequirementJobDescription', 'ActualRequirement', 'RequirementSkills', 'RequirementZIPCode',
+                        'CreatedDate', 'ModifiedDate']
+
+        with self.connection.cursor() as cursor:
+            res = cursor.execute("{CALL Usp_ML_Get_RequirementlistByDates (?,?,?)}",
+                                 (last_executed_time, start_modified_time, end_modified_time))
+            result = res.fetchall()
+
+        result = np.array([np.array(r) for r in result])
+        df = pd.DataFrame(result, columns=column_names)
+
         if 'Unnamed: 0' in df:
             df.drop(labels=['Unnamed: 0'], axis=1, inplace=True)
 
