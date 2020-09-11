3fd0db40d1f838d4c14d6d78d4c2cb64a77b4c10
Mohit Thakkar <mohitthakkar@rangam.com>
2020-09-11 16:04:37 +0530

    Add Error Logging

--- commander/tasks.py
+++ commander/tasks.py
@@ -1,12 +1,15 @@
 __author__ = "Mohit Thakkar"
 
 from django.core import management
+from datetime import datetime
 
 from celery.schedules import crontab
 from celery.task import periodic_task
 from celery.utils.log import get_task_logger
 
-logger = get_task_logger(__name__)
+from knight.db_scripts import logger
+
+celery_logger = get_task_logger(__name__)
 
 
 @periodic_task(
@@ -18,7 +21,10 @@ def task_test():
     """
     Test task
     """
-    print('Test task that runs every minute!!!')
-    # management.call_command('fetch_all_candidates')
-    # logger.info("Test task printed time!")
+    try:
+        print('Test task that runs every minute!!!')
+        # management.call_command('fetch_all_candidates')
+        # celery_logger.info("Test task printed time!")
+    except Exception as e:
+        logger.log('(' + str(datetime.now()) + ') ERROR: ' + str(e))
     return
---
+++ knight/db_scripts/logger.py
@@ -0,0 +1,14 @@
+__author__ = "Mohit Thakkar"
+
+import pyodbc
+from django.conf import settings
+
+
+def log(data):
+    connection = pyodbc.connect(DRIVER=settings.DB_DRIVER,
+                                SERVER=settings.DB_SERVER,
+                                DATABASE=settings.DB_NAME,
+                                UID=settings.DB_USER,
+                                PWD=settings.DB_PASSWORD)
+    with connection.cursor() as cursor:
+        cursor.execute("Insert into ProcessLog (Data) VALUES (?)", ('[Artie 2.0] ' + str(data)))
